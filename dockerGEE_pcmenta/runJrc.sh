1efd35bcfadf845cc2c319011778f813f70f0043
Lorenzo Mentaschi <lorenzo.mentaschi@unige.it>
2020-06-04 15:03:28 +0200

    docker for gee

---
+++ dockerGEE_pcmenta/runJrc.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --dns 10.191.240.1 -v /home/lmentaschi/workspace/docker0/:/home/lmentaschi  --workdir=/home/lmentaschi gee_pcmenta
---
+++ dockerGEE_pcmenta/runNormal.sh
@@ -0,0 +1,1 @@
+docker run -it --rm  -v /home/lmentaschi/workspace/docker_gee_pcmenta/:/home/lmentaschi -u lmentaschi --workdir=/home/lmentaschi --name=gee gee_pcmenta bash
