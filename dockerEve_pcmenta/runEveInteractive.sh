2e4ac6126acf4b4fa43a9a17eba40ba73b717d13
Lorenzo Mentaschi <lorenzo.mentaschi@unige.it>
2020-11-07 17:33:35 +0100

    docker eve, in the run script, increased the shared memory size

--- dockerEve_pcmenta/runEveInteractive.sh
+++ dockerEve_pcmenta/runEveInteractive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 EHOME=$HOME/workspace/docker_eve/
 xhost +
-#docker run -it --rm -e DISPLAY=$DISPLAY --device=/dev/dri --mount type=bind,source="$EHOME",target="$HOME" -v /tmp/.X11-unix/:/tmp/.X11-unix/  -u $USER --workdir=$HOME --env="APP=glxgears" --privileged  --name=eve  eveonlineimg bash
-docker run -it --rm -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri -v /tmp/.X11-unix/:/tmp/.X11-unix/ --mount type=bind,source="$EHOME",target="$HOME"  -u $USER --workdir=$HOME  --name=eve  eveonlineimg bash
+# the param --shm-size=256m increases the shared memory size to 256m (64m is the default), to avoid strange crashes of the launcher
+docker run -it --rm -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri -v /tmp/.X11-unix/:/tmp/.X11-unix/ --mount type=bind,source="$EHOME",target="$HOME"  -u $USER --workdir=$HOME  --name=eve --shm-size=256m  eveonlineimg bash
