1f42989248bc5c898f60343dff2d801520d4d6ef
Lorenzo Mentaschi <lorenzo.mentaschi@unige.it>
2020-11-14 17:05:51 +0100

    eve docker: launchers for 2 players

--- dockerEve_pcmenta/runEveInteractive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-EHOME=$HOME/workspace/docker_eve/
-xhost +
-# the param --shm-size=256m increases the shared memory size to 256m (64m is the default), to avoid strange crashes of the launcher
-docker run -it --rm -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri -v /tmp/.X11-unix/:/tmp/.X11-unix/ --mount type=bind,source="$EHOME",target="$HOME"  -u $USER --workdir=$HOME  --name=eve --shm-size=256m  eveonlineimg bash
---
+++ dockerEve_pcmenta/runEvePlayer1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+EHOME=$HOME/workspace/docker_eve_player1/
+xhost +
+# the param --shm-size=256m increases the shared memory size to 256m (64m is the default), to avoid strange crashes of the launcher
+docker run -it --rm -e DISPLAY=$DISPLAY --device=/dev/dri:/dev/dri -v /tmp/.X11-unix/:/tmp/.X11-unix/ --mount type=bind,source="$EHOME",target="$HOME"  -u $USER --workdir=$HOME  --name=evePlayer1 --shm-size=256m  eveonlineimg bash
